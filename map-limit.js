2a11b8cdbc26ea208d53ed8f31db9be5da79250d
mohammadRaiyyan <inforaiyyan@gmail.com>
2026-08-04 14:25:02 +0530

    Added mapLimit concurrent task execution

---
+++ map-limit.js
@@ -0,0 +1,74 @@
+/*
+Implement mapLimit, which is a utility function which produces a list of outputs by mapping
+each inputs through an itrette function.
+Inputs:
+  inputs:An array of inputs
+  limit: The number of maximum operation can be performed at a time
+  iterateeFn: The async function that will be called with each input to generate the corresponding output. it will have
+              two args:
+              input: The input being processed
+              callback: A function that will be called when the input is finished processing.
+                        It will be provided one argument at a time.
+
+  callback: A function that should be called with the array of outputs once all the operations are performed.
+
+*/
+
+async function mapLimit(inputs, limit, iterateeFn, callback) {
+  const results = [];
+  let runningTasks = new Set();
+  for (const [index, input] of inputs.entries()) {
+    console.log(`🚀 Starting ${input}`);
+    const task = new Promise((resolve, reject) => {
+      function cleanup(e) {
+        runningTasks.delete(task);
+        results[index] = undefined;
+        reject(e);
+      }
+      try {
+        iterateeFn(input, (error, res) => {
+          if (error) {
+            cleanup(error);
+            return;
+          }
+
+          console.log(`✅ Finished ${input}`);
+          results[index] = res;
+
+          runningTasks.delete(task);
+          resolve(res);
+          console.log(
+            `Running: ${runningTasks.size} | Remaining: [${[...runningTasks].length}]`,
+          );
+        });
+      } catch (error) {
+        cleanup(error);
+      }
+    });
+
+    runningTasks.add(task);
+
+    console.log(`📌 Added ${input}, Currently Running = ${runningTasks.size}`);
+
+    if (runningTasks.size >= limit) {
+      console.log(`⏸ Limit reached (${limit}). Waiting...\n`);
+      await Promise.any(runningTasks);
+      console.log(`▶ One task completed. Continuing...\n`);
+    }
+  }
+  await Promise.all(runningTasks);
+  callback(results);
+}
+
+async function iterateeFn(input, callback) {
+  const randomDelay = Math.floor(Math.random() * 100) + 200;
+  setTimeout(() => {
+    const result = input * 2;
+    callback(null, result);
+  }, randomDelay);
+}
+function processedcallback(results) {
+  console.log(results);
+}
+
+mapLimit([1, 2, 3, 4, 5], 3, iterateeFn, processedcallback);
