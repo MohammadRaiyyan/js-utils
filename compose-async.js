f37f2b1e00b17c2c810579e9047ce1ffb387cf5a
mohammadRaiyyan <inforaiyyan@gmail.com>
2026-08-11 23:29:49 +0530

    Added problem statement boilerplate for compose async function

---
+++ compose-async.js
@@ -0,0 +1,23 @@
+function a(x, y) {
+  return new Promise((resolve) => setTimeout(() => resolve(x * y), 100));
+}
+
+function b(z) {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => resolve(z + 5), 100),
+  );
+}
+
+function c(r) {
+  return new Promise((resolve) => setTimeout(() => resolve(r / 10), 100));
+}
+
+composeAsync(
+  c,
+  b,
+  a,
+)(5, 3)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch(console.error);
