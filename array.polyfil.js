6966a551d020d28e89a41354edc8229a1cdfade8
mohammadRaiyyan <inforaiyyan@gmail.com>
2026-08-17 21:31:57 +0530

    Implemented Array reduce method polyfil

--- array.polyfil.js
+++ array.polyfil.js
@@ -46,3 +46,45 @@ console.log(
     return item % 2 === 0;
   }),
 );
+
+// Reduce
+
+Array.prototype.myReduce = function (callback, initialValue) {
+  if (typeof callback !== "function") {
+    throw new TypeError(
+      `Expected callback function as an argument but got ${typeof callback}`,
+    );
+  }
+  const arr = this;
+  let acc = initialValue ?? this[0];
+  let startIndex = initialValue != undefined ? 0 : 1;
+  for (let i = startIndex; i < arr.length; i++) {
+    acc = callback(acc, arr[i], i, arr);
+  }
+  return acc;
+};
+
+console.log(
+  "Reduce sum result: ",
+  [1, 2, 3].myReduce((acc, item, index, arr) => {
+    return acc + item;
+  }, 0),
+);
+console.log(
+  "Reduce sum without initial result: ",
+  [1, 2, 3].myReduce((acc, item, index, arr) => {
+    return acc + item;
+  }),
+);
+console.log(
+  "Reduce group result: ",
+  [1, 2, 3].myReduce((acc, item, index, arr) => {
+    const isEven = item % 2 === 0;
+    if (isEven) {
+      acc.isEven = acc.isEven ? [...acc.isEven, item] : [item];
+    } else {
+      acc.isOdd = acc.isOdd ? [...acc.isOdd, item] : [item];
+    }
+    return acc;
+  }, {}),
+);
