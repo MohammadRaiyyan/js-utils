c017ff8379615a15de363d25d0bbce8f8aea46ea
mohammadRaiyyan <inforaiyyan@gmail.com>
2026-08-17 21:08:04 +0530

    Implemented array filter polyfil

--- array.polyfil.js
+++ array.polyfil.js
@@ -20,3 +20,29 @@ console.log(
     return item * 2;
   }),
 );
+
+// Filter
+
+Array.prototype.myFilter = function (callback) {
+  if (typeof callback !== "function") {
+    throw new TypeError(
+      `Expected callback function as an argument but got ${typeof callback}`,
+    );
+  }
+  const result = [];
+  const arr = this;
+  for (let i = 0; i < arr.length; i++) {
+    const isTruthy = callback(arr[i], i, arr);
+    if (isTruthy) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+console.log(
+  "Filter result: ",
+  [1, 2, 3].myFilter((item, index, arr) => {
+    return item % 2 === 0;
+  }),
+);
